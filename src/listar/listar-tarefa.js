2012800338c92bc2b32c2b196e01d2a92e7ae6bb
Tiago Mendes <tiago.mendes@dsbsolucoes.com.br>
2021-01-13 13:20:59 -0300

    Paginacao

--- src/listar/listar-tarefa.js
+++ src/listar/listar-tarefa.js
@@ -4,17 +4,24 @@ import { Table } from 'react-bootstrap'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import ItemsListarTarefas from './items-listar-tarefas'
+import Paginacao from './paginacao'
 
 function ListarTarefa() {
 
+    const ITENS_POR_PAGINA = 3
+
     const [tarefas, setTarefas] = useState([])
     const [carregaTarefas, setCarregaTarefas] = useState(true)
+    const [totalItems, setTotalItens] = useState(0)
+    const [paginaAtual, setPaginaAtual] = useState(1)
 
     useEffect(() => {
         function obterTarefas() {
             const tarefasDB = localStorage['tarefas']
             let listarTarefas = tarefasDB ? JSON.parse(tarefasDB) : []
-            setTarefas(listarTarefas)
+            setTotalItens(listarTarefas.length)
+
+            setTarefas(listarTarefas.splice((paginaAtual - 1) * ITENS_POR_PAGINA, ITENS_POR_PAGINA))
         }
         if (carregaTarefas) {
             obterTarefas()
@@ -22,6 +29,10 @@ function ListarTarefa() {
         }
     }, [carregaTarefas])
 
+    function handleMudarPagina(pagina) {
+        setPaginaAtual(pagina)
+        setCarregaTarefas(true)
+    }
 
     return (
         <div className="text-center">
@@ -45,6 +56,10 @@ function ListarTarefa() {
                     <ItemsListarTarefas tarefas={tarefas} recarregarTarefas={setCarregaTarefas} />
                 </tbody>
             </Table>
+            <Paginacao totalItems={totalItems}
+                itensPorPagina={ITENS_POR_PAGINA}
+                paginaAtual={paginaAtual}
+                mudarPagina={handleMudarPagina}></Paginacao>
         </div>
     )
 }
